865cf0e7664613bed8192b81eb62ce46b60cfc16
iqbalululazmi <iqbalululazmi@gmail.com>
2020-02-15 09:49:06 +0700

    update project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-autocorrect app is running!');
+    expect(page.getTitleText()).toEqual('angular-autocomplete app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-autocorrect'`, () => {
+  it(`should have as title 'angular-autocomplete'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-autocorrect');
+    expect(app.title).toEqual('angular-autocomplete');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-autocorrect app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-autocomplete app is running!');
   });
 });
